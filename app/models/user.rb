3079e2d90d9603e7c72ccf8836d19c8d01ad40d6
shota4423 <shota.web.create@gmail.com>
2026-04-08 11:04:06 +0900

    add: Likeモデルの作成と各modelの紐づけ

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,6 +24,7 @@ class User < ApplicationRecord
 
   has_many :articles, dependent: :destroy
   has_one :profile, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
   delegate :age, :gender, to: :profile, allow_nil: true
 

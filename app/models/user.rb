79e649c4ff445bf4997cec54dc7067224700793f
shota4423 <shota.web.create@gmail.com>
2026-04-26 22:27:02 +0900

    add: フォローを外す機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,6 +30,9 @@ class User < ApplicationRecord
   has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
   has_many :followings, through: :following_relationships, source: :following
 
+  has_many :follower_relationships, foreign_key: 'following_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followers, through: :follower_relationships, source: :follower
+
   delegate :age, :gender, to: :profile, allow_nil: true
 
   def has_written?(article)
@@ -47,6 +50,11 @@ class User < ApplicationRecord
 
   def follow!(user)
     following_relationships.create!(following_id: user.id)
+  end
+
+  def unfollow!(user)
+    relation = following_relationships.find_by!(following_id: user.id)
+    relation.destroy!
   end
 
   def prepare_profile

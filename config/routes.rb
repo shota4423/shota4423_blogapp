99af4059bb1a2e9a8715c636079c07f17a1fb032
shota4423 <shota.web.create@gmail.com>
2026-04-28 13:50:39 +0900

    add: タイムラインのルーティング設定、showアクション初期設定、最低限のview設定

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root to: 'articles#index'
+  resource :timeline, only: [:show]
 
   resources :articles do
     resources :comments, only: [:new, :create]

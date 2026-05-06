237476f24bce8c1be41966a2d375ddfa3bb3aabd
shota4423 <shota.web.create@gmail.com>
2026-05-06 22:33:26 +0900

    add: JSモジュール･いいね表示の切り替え

--- app/javascript/article.js
+++ app/javascript/article.js
@@ -1,7 +1,10 @@
 import { $ as jquery } from "jquery"
 import axios from "axios"
-
 import Rails from "@rails/ujs"
+import {
+  listenInactiveHeartEvent,
+  listenActiveHeartEvent
+} from './modules/handle_heart'
 
 axios.defaults.headers.common['X-CSRF-Token'] = Rails.csrfToken()
 
@@ -40,6 +43,9 @@ document.addEventListener('turbo:load', () => {
         appendNewComment(comment)
       })
     })
+    .catch((error) => {
+      window.alert('sippai')
+    })
 
   handleCommentForm()
 
@@ -64,32 +70,6 @@ document.addEventListener('turbo:load', () => {
       const hasLiked = response.data.hasLiked
       handleHeartDisplay(hasLiked)
     })
-
-  $('.inactive-heart').on('click', () => {
-    axios.post(`/articles/${articleId}/like`)
-      .then((response) => {
-        if (response.data.status === 'ok') {
-            $('.active-heart').removeClass('hidden')
-            $('.inactive-heart').addClass('hidden')
-        }
-      })
-      .catch((e) => {
-        window.alert('NG')
-        console.log('ngngng')
-      })
-  })
-
-  $('.active-heart').on('click', () => {
-    axios.delete(`/articles/${articleId}/like`)
-      .then((response) => {
-        if (response.data.status === 'ok') {
-            $('.inactive-heart').removeClass('hidden')
-            $('.active-heart').addClass('hidden')
-        }
-      })
-      .catch((e) => {
-        window.alert('NG')
-        console.log('ngngng')
-      })
-  })
+  listenInactiveHeartEvent(articleId)
+  listenActiveHeartEvent(articleId)
 })

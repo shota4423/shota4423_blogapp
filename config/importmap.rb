30058867d11316fd4ed95436db2647600951e94f
shota4423 <shota.web.create@gmail.com>
2026-05-06 23:25:00 +0900

    fix: axiosのCDN404を修正

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,7 +8,7 @@ pin_all_from 'app/javascript/controllers', under: 'controllers'
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.esm.js"
 pin "jquery", to: "https://esm.sh/jquery@4.0.0"
-pin "axios", to: "https://esm.sh/axios"
+pin "axios", to: "https://cdn.jsdelivr.net/npm/axios@1.7.2/+esm"
 pin "@rails/ujs", to: "@rails--ujs.js" # @7.1.3
 pin "article"
 pin_all_from "app/javascript/modules", under: "modules"

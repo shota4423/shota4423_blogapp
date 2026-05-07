ed0a4a8d0dd03187a942a0db0acd0c8e145646aa
shota4423 <shota.web.create@gmail.com>
2026-05-07 20:53:59 +0900

    add: mailerの作成と送信機能の実装

---
+++ app/mailers/relationship_mailer.rb
@@ -0,0 +1,11 @@
+class RelationshipMailer < ApplicationMailer
+  def new_follower(user, follower)
+    @user = user
+    @follower = follower
+    mail to: user.email, subject: '【お知らせ】フォローされました'
+  end
+
+  def welcome
+
+  end
+end

81de45858e7b39cd93d6f328f56dacc0531abe26
shota4423 <shota.web.create@gmail.com>
2026-05-11 22:44:27 +0900

    add: namespace, scopeに従いcontrollerを変更

--- config/routes.rb
+++ config/routes.rb
@@ -20,11 +20,7 @@ Rails.application.routes.draw do
   root to: 'articles#index'
   resource :timeline, only: [:show]
 
-  resources :articles do
-    resources :comments, only: [:new, :index, :create]
-
-    resource :like, only: [:show, :create, :destroy]
-  end
+  resources :articles
 
   resources :accounts, only: [:show] do
     resources :follows, only: [:create]
@@ -33,4 +29,11 @@ Rails.application.routes.draw do
 
   resource :profile, only: [:show, :edit, :update]
   resources :favorites, only: [:index]
+
+  namespace :api, default: { format: :json } do
+    scope '/articles/:article_id' do
+      resources :comments, only: [:index, :create]
+      resource :like, only: [:show, :create, :destroy]
+    end
+  end
 end

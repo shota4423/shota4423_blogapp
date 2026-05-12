c6ae3ee85105d2d9229f73c47b3d89fc7bfcdef4
shota4423 <shota.web.create@gmail.com>
2026-05-12 23:31:52 +0900

    add: letを使ったテスト記法と保存できない場合のテスト

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,16 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  it 'タイトルと内容が入力されていれば、記事を保存できる' do
-    user = User.create!({
+  let!(:user) do
+    User.create!({
       email: 'test@example.com',
       password: 'password'
     })
-    article = user.articles.build({
-      title: Faker::Lorem.characters(number: 10),
-      content: Faker::Lorem.characters(number: 300)
-    })
+  end
 
-    expect(article).to be_valid
+  context 'タイトルと内容が入力されている場合' do
+    let!(:article) do
+      user.articles.build({
+        title: Faker::Lorem.characters(number: 10),
+        content: Faker::Lorem.characters(number: 300)
+      })
+    end
+
+    it '記事を保存できる' do
+      expect(article).to be_valid
+    end
+  end
+
+  context 'タイトルの文字が一文字の場合' do
+    let!(:article) do
+      user.articles.create({
+        title: Faker::Lorem.characters(number: 1),
+        content: Faker::Lorem.characters(number: 300)
+      })
+    end
+
+    it '記事が保存できない' do
+      expect(article.errors.messages[:title][0]).to eq('は2文字以上で入力してください')
+    end
   end
 end
